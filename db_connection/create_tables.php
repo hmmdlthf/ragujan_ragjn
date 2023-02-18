115df3063df579ac7a6739accdccd77f4854d48f
kevin_jorn <Althaf>
2023-02-19 00:55:48 +0530

    change

--- db_connection/create_tables.php
+++ db_connection/create_tables.php
@@ -1,18 +1,18 @@
 <?php 
 
-$sql1 = readfile("sql.sql");
-$sql2 = readfile("sql2.sql");
-$sql3 = readfile("sql3.sql");
+// $sql1 = readfile("sql.sql");
+// $sql2 = readfile("sql2.sql");
+// $sql3 = readfile("sql3.sql");
 
-$create_tables = new Db();
-$statement = $create_tables->connect()->prepare($sql1);
-$statement->execute([]);
+// $create_tables = new Db();
+// $statement = $create_tables->connect()->prepare($sql1);
+// $statement->execute([]);
 
-$statement2 = $create_tables->connect()->prepare($sql2);
-$statement2->execute([]);
+// $statement2 = $create_tables->connect()->prepare($sql2);
+// $statement2->execute([]);
 
-$statement3 = $create_tables->connect()->prepare($sql3);
-$statement3->execute([]);
+// $statement3 = $create_tables->connect()->prepare($sql3);
+// $statement3->execute([]);
 
 echo "successfully executed all the sql";
 ?>
