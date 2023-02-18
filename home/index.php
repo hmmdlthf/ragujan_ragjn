043582cc94709b497d02400853055137d75a6efa
kevin_jorn <Althaf>
2023-02-18 22:12:22 +0530

    change

--- home/index.php
+++ home/index.php
@@ -15,10 +15,14 @@ $audio_sample_display_page_shortend = GlobalLinkFiles::getRelativePath("audio_sa
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?=$style_path?>home.css">
     <link rel="stylesheet" href="<?=$style_path?>navbar.css">
     <title>BeatSample</title>
 
+    <link rel="stylesheet" href="<?=$style_path?>bootstrap.css">
     <?php include "/head/link_tags.php" ?>
 </head>
 
--- index.php
+++ index.php
@@ -10,14 +10,21 @@ echo $authenticate_download_url;
 <!DOCTYPE html>
 
 <html lang="en">
+
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+    <link rel="stylesheet" href="<?=$style_path?>bootstrap.css">
     <?php include "/head/link_tags.php" ?>
 </head>
+
 <body>
-   <script>
-    window.location = "/home/index.php";
-   </script> 
+    <script>
+        window.location = "/home/index.php";
+    </script>
 </body>
+
 </html>
