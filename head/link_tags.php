a1ca74618349ce28482a80c1b2d26914541b3f00
kevin_jorn <Althaf>
2023-02-18 22:14:18 +0530

    change

--- head/link_tags.php
+++ head/link_tags.php
@@ -1,3 +1,3 @@
 <link rel="shortcut icon" href="/resources//icon_images//logo_transparent.png" type="image/x-icon">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css"> -->
 
